149d0c400efbe55a1396f3b6e3e47217d8fbfeac
higgs1022 <csh8130@kakao.com>
2022-06-20 16:31:51 +0900

    chore: chessgame test target 생성

---
+++ swift-chessgame/swift-chessgameTests/swift_chessgameTests.swift
@@ -0,0 +1,16 @@
+//
+//  swift_chessgameTests.swift
+//  swift-chessgameTests
+//
+//  Created by main on 2022/06/20.
+//
+
+import XCTest
+
+class swift_chessgameTests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
