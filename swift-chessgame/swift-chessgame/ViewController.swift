fee7d49aa6b0d53379b830e4e1122ce69c3f6a0d
higgs1022 <csh8130@kakao.com>
2022-06-29 02:11:18 +0900

    feat: IBAction 몇번째 좌표를 클릭했는지 검출

--- swift-chessgame/swift-chessgame/ViewController.swift
+++ swift-chessgame/swift-chessgame/ViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var verticalStack: UIStackView!
+    @IBOutlet weak var horizontalStack0: UIStackView!
+    @IBOutlet weak var horizontalStack1: UIStackView!
+    @IBOutlet weak var horizontalStack2: UIStackView!
+    @IBOutlet weak var horizontalStack3: UIStackView!
+    @IBOutlet weak var horizontalStack4: UIStackView!
+    @IBOutlet weak var horizontalStack5: UIStackView!
+    @IBOutlet weak var horizontalStack6: UIStackView!
+    @IBOutlet weak var horizontalStack7: UIStackView!
+    
+    
     let board = Board()
 
     override func viewDidLoad() {
@@ -15,6 +26,101 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pieceTouch(_ sender: UIButton) {
+        var x = -1
+        var y = -1
+        
+        if horizontalStack0.contains(sender) {
+            y = 1
+            horizontalStack0
+                .arrangedSubviews
+                .enumerated()
+                .forEach { index, element in
+                    if element == sender {
+                        x = index + 1
+                    }
+                }
+        }
+        if horizontalStack1.contains(sender) {
+            y = 2
+            horizontalStack1
+                .arrangedSubviews
+                .enumerated()
+                .forEach { index, element in
+                    if element == sender {
+                        x = index + 1
+                    }
+                }
+        }
+        if horizontalStack2.contains(sender) {
+            y = 3
+            horizontalStack2
+                .arrangedSubviews
+                .enumerated()
+                .forEach { index, element in
+                    if element == sender {
+                        x = index + 1
+                    }
+                }
+        }
+        if horizontalStack3.contains(sender) {
+            y = 4
+            horizontalStack3
+                .arrangedSubviews
+                .enumerated()
+                .forEach { index, element in
+                    if element == sender {
+                        x = index + 1
+                    }
+                }
+        }
+        if horizontalStack4.contains(sender) {
+            y = 5
+            horizontalStack4
+                .arrangedSubviews
+                .enumerated()
+                .forEach { index, element in
+                    if element == sender {
+                        x = index + 1
+                    }
+                }
+        }
+        if horizontalStack5.contains(sender) {
+            y = 6
+            horizontalStack5
+                .arrangedSubviews
+                .enumerated()
+                .forEach { index, element in
+                    if element == sender {
+                        x = index + 1
+                    }
+                }
+        }
+        if horizontalStack6.contains(sender) {
+            y = 7
+            horizontalStack6
+                .arrangedSubviews
+                .enumerated()
+                .forEach { index, element in
+                    if element == sender {
+                        x = index + 1
+                    }
+                }
+        }
+        if horizontalStack7.contains(sender) {
+            y = 8
+            horizontalStack7
+                .arrangedSubviews
+                .enumerated()
+                .forEach { index, element in
+                    if element == sender {
+                        x = index + 1
+                    }
+                }
+        }
+        
+        print("\(x) \(y)")
+    }
+    
 }
 
